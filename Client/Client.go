9990742751a0da3b6a18f256935de422ab787ff1
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2022-01-05 10:15:00 +0100

    Client implemented in accordance with specification.

--- Client/Client.go
+++ Client/Client.go
@@ -5,11 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var target = flag.String("target", ":5080", "target")
-
-var operations = make(map[string]func())
 
 func main() {
 	flag.Parse()
@@ -19,34 +19,49 @@ func main() {
 		fe.conn.Close()
 	}()
 
-	operations["q"] = func() {
-		os.Exit(0)
-	}
-	operations["inc"] = func() {
-		val := fe.Increment()
-		fmt.Printf("New value is %d.\n", val)
-	}
-
-	fmt.Printf("Type \"inc\" to increment the value in the system.\n")
+	fmt.Printf("Type \"set x y\" to assign the value y, to the key x in the system.\n")
+	fmt.Printf("Type \"get x\" to retrieve the value stored at x.\n")
 	fmt.Printf("Type \"q\" to exit the program.\n")
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		text, _ := reader.ReadString('\n')
 		text = text[:len(text)-2] // Cut of newline character.
+		text = strings.ToLower(text)
 
-		if contains(operations, text) {
-			operations[text]()
-		} else {
+		sep := strings.Split(text, " ")
+
+		switch sep[0] {
+		case "q":
+			os.Exit(0)
+		case "get":
+			if len(sep) != 1 {
+				fmt.Println("Incorrect number of arguments for command \"get\". Expected 1.")
+				break
+			}
+			x, err := strconv.Atoi(sep[1])
+			if err != nil {
+				fmt.Println("x must be an integer.")
+				break
+			}
+			fmt.Printf("%d\n", fe.Get(x))
+		case "set":
+			if len(sep) != 2 {
+				fmt.Println("Incorrect number of arguments for command \"set\". Expected 2.")
+				break
+			}
+			x, err1 := strconv.Atoi(sep[1])
+			y, err2 := strconv.Atoi(sep[2])
+			if err1 != nil || err2 == nil {
+				fmt.Println("x and y must be integers.")
+				break
+			}
+			if fe.Set(x, y) {
+				fmt.Println("Value successfully stored.")
+			} else {
+				fmt.Println("Failed to store value.")
+			}
+		default:
 			fmt.Println("Unknown command.")
 		}
 	}
-}
-
-func contains(m map[string]func(), key string) bool {
-	for k, _ := range m {
-		if k == key {
-			return true
-		}
-	}
-	return false
 }
